53d2e7a7f299bdcf84b4523b8c664e6b5f879531
NickooMar <nicoo.marsili@gmail.com>
2024-05-08 20:55:11 -0300

    fix: versioning & .env

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { config } from './config';
+import { VersioningType } from '@nestjs/common/enums';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(config.port || 4000);
 }
 
