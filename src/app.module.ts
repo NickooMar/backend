8a3d8e2d128b3ec180afc1e5b1fbdc8dbb6c0fca
NickooMar <nicoo.marsili@gmail.com>
2024-05-08 16:06:08 -0300

    chore: enviroments variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import configuration from './config/configuration';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NewslettersModule } from './modules/newsletters/newsletters.module';
+import { config } from './config';
+
+const { host, port, username, password, database } = config.typeOrmConfig;
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env', '.env.development'],
-      isGlobal: true,
-      load: [configuration],
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService) => ({
-        type: 'postgres',
-        host: configService.get('database.host'),
-        port: parseInt(configService.get('database.port')),
-        username: configService.get('database.username'),
-        password: configService.get('database.password'),
-        database: configService.get('database.database'),
-        ssl: configService.get('database.ssl'),
-        synchronize: true,
-      }),
-      inject: [ConfigService],
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host,
+      port,
+      username,
+      password,
+      database,
     }),
     NewslettersModule,
   ],
---
+++ src/config.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+
+class Config {
+  get appKey() {
+    return this.getEnv('APP_KEY');
+  }
+
+  get typeOrmConfig() {
+    return {
+      host: this.getEnv('DATABASE_HOST'),
+      port: Number(this.getEnv('DATABASE_PORT')),
+      username: this.getEnv('DATABASE_USERNAME'),
+      password: this.getEnv('DATABASE_PASSWORD'),
+      database: this.getEnv('DATABASE_NAME'),
+    };
+  }
+
+  getEnv(key: string) {
+    if (!process.env[key]) {
+      throw new Error(`Environment variable ${key} is not set`);
+    }
+    return process.env[key];
+  }
+}
+
+export const config = new Config();
